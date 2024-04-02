220abda160173d9b1e1081b3e68f8855f5b8d385
Raimundo braz <wilsonquilenguebraz@gmail.com>
2024-04-02 11:56:00 +0100

    criando modal

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const fileInput = document.getElementById('fileInput');
+  const imagePreview = document.getElementById('imagePreview');
+
+  fileInput.addEventListener('change', function() {
+    const file = this.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function(event) {
+        imagePreview.querySelector('img').setAttribute('src', event.target.result);
+      };
+      reader.readAsDataURL(file);
+    } else {
+      imagePreview.querySelector('img').setAttribute('src', '#');
+    }
+  });
